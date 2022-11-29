7fe30b14b4c51c18c009cf4259b924fd9be5e03c
NatanCavasin <natancavasin@hotmail.com>
2022-11-29 18:55:59 -0300

    Initial Commit

---
+++ figma-tokens/build/js/tokens.js
@@ -0,0 +1,12 @@
+/**
+ * Do not edit directly
+ * Generated on Tue, 29 Nov 2022 21:52:42 GMT
+ */
+
+module.exports = {
+  "ColorWhite": "#ffffff",
+  "ColorGray500": "#50505c",
+  "ColorGray800": "#1c1c1f",
+  "ColorGray900": "#121214",
+  "ColorPurple500": "#8257e6"
+};
